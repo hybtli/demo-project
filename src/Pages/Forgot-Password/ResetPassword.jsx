4d8f8dbd3eca4f98824c9d4f19dbdcc5502ed2c9
Nurbala Heybatov <nurbala788788@gmail.com>
2022-07-13 13:18:34 +0400

    Password validation is added to 'Reset Password' screen

--- src/Pages/Forgot-Password/ResetPassword.jsx
+++ src/Pages/Forgot-Password/ResetPassword.jsx
@@ -9,6 +9,25 @@ export default function ResetPassword() {
   const navigate = useNavigate();
   const axios = require("axios");
 
+  //Definitions for password validation begin here :
+
+  // RegEx definitions :
+  const uppercaseRegExp = /(?=.*?[A-Z])/;
+  const lowercaseRegExp = /(?=.*?[a-z])/;
+  const digitsRegExp = /(?=.*?[0-9])/;
+  const specialCharRegExp = /(?=.*?[#?!@$%^&*-])/;
+  const lengthRegExp = /.{8,20}/;
+
+  const uppercasePassword = uppercaseRegExp.test(inputs.password);
+  const lowercasePassword = lowercaseRegExp.test(inputs.password);
+  const digitsPassword = digitsRegExp.test(inputs.password);
+  const specialCharPassword = specialCharRegExp.test(inputs.password);
+  const lengthPassword = lengthRegExp.test(inputs.password);
+
+  let errorMessage = "";
+
+  //RegEx definitions for password validation finish here :
+
   const handleChange = (event) => {
     const name = event.target.name;
     const value = event.target.value;
@@ -19,19 +38,34 @@ export default function ResetPassword() {
     event.preventDefault();
     if (inputs.password !== inputs.confirmPassword) {
       toast.error("Parol blәt !!!");
+    } else {
+      axios
+        .post(
+          `${apiURL.url}/api/reset-password?forgetPasswordToken=${inputs.token}&password=${inputs.password}`
+        )
+        .then((response) => {
+          navigate("/login");
+        })
+        .catch((err) => {
+          const message = err.response.data.message;
+          if (message === "Password is not acceptable") {
+            if (!uppercasePassword) {
+              errorMessage = "At least one uppercase letter";
+            } else if (!lowercasePassword) {
+              errorMessage = "At least one lowercase";
+            } else if (!digitsPassword) {
+              errorMessage = "At least one digit";
+            } else if (!specialCharPassword) {
+              errorMessage = "At least one special characters";
+            } else if (!lengthPassword) {
+              errorMessage = "At least 8, at most 20 characters";
+            }
+            toast.error(errorMessage);
+          } else {
+            toast.error(message);
+          }
+        });
     }
-
-    axios
-      .post(
-        `${apiURL.url}/api/reset-password?forgetPasswordToken=${inputs.token}&password=${inputs.password}`
-      )
-      .then((response) => {
-        navigate("/login");
-      })
-      .catch((err) => {
-        const message = err.response.data.message;
-        toast.error(message);
-      });
   };
 
   return (
